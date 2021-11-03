c089323fb458f55e95bad7e519e94bf0d291eb80
Anthony Hill <hillganthony@outlook.com>
2021-11-03 13:06:09 -0400

    commit tree 3cbd85593a4d051888f564d18190b574ada84b47

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,24 @@
 class Dog
+    def name=(name)
+        @dogs_name = name
+        
+        def name
+        @dogs_name
+        end
+            def breed=(breed)
+                @dogs_breed = breed
+            end
 
+                 def breed
+                    @dogs_breed
+                 end
+
+    end
 end
+
+airbud = Dog.new
+airbud
+airbud.name = "Airbud"
+
+
+puts airbud.name
